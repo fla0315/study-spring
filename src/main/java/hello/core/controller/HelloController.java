919c6abe9fadc86472e6c1f4f6077e46413bd22a
김영림 <kimbloom56@gmail.com>
2024-03-16 11:52:01 +0900

    [rimi] git push

--- src/main/java/hello/core/controller/HelloController.java
+++ src/main/java/hello/core/controller/HelloController.java
@@ -9,6 +9,7 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
+        //key , data
         model.addAttribute("data", "hello!!!");
         return "hello";
     }
