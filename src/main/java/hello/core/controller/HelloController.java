65e18efbdbf8d247aaf54596eb503974d8d20964
김영림 <kimbloom56@gmail.com>
2024-03-17 13:07:16 +0900

    [rimi] 섹션 3 진행 중

--- src/main/java/hello/core/controller/HelloController.java
+++ src/main/java/hello/core/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.core.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -11,7 +13,41 @@ public class HelloController {
     public String hello(Model model) {
         //key , data
         model.addAttribute("data", "hello!!!");
-        return "hello";
+        return "hello"; //해당 이름 화면을 실행시켜라.
     }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+
+    //이 방법은 거의 안씀
+    @GetMapping("hello-string")
+    @ResponseBody //http body에 이 내용을 직접 넣어주겠다.
+    public String helloString (@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi (@RequestParam("name")String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 
 }
